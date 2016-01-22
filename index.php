351fa02b91d637e1f0879102fca43d0f1e29bdf0
paliwalvijay <paliwal.2@iitj.ac.in>
2016-01-22 22:26:22 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+ <title>MyFirstBlog</title>
+</head>
+<body style="background:#2B65EC">
+  <h1>Welcome to MyBlog</h1>
+<form name="htmlform" method="post" action="login.php">
+<table width="450px">
+</tr>
+<tr>
+ <td valign="top">
+  <label for="email" style="color: #FFFFFF"><b>Email </b></label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="email" maxlength="80" size="30">
+ </td>
+ 
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="password" style="color: #FFFFFF"><b>Password</b></label>
+ </td>
+ <td valign="top">
+  <input  type="password" name="password" maxlength="30" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td colspan="2" style="text-align:center">
+  <input type="submit" value="Log In">   ( <a href="signup.php">Sign Up</a> )
+ </td>
+</tr>
+</table>
+</form>
+</body>
+ </html>
